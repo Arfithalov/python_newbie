d415f6962dc477d2df9c5f968ca5ebd9882d26d2
Arfithalov <Arfithalov@yandex.ru>
2018-09-26 17:03:50 +0300

    Changes made in odd.py (repetition).

--- odd.py
+++ odd.py
@@ -1,14 +1,21 @@
 from datetime import datetime
+import random
+import time
 odds = [1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23, 25, 27, 29, 31, 33, 35, 37, 39, 41, 43, 45, 47, 49, 51, 53, 55, 57, 59]
-right_this_minute = datetime.today().minute
-if right_this_minute in odds:
-    print("This minute seems odd.")
-else:
-    print("Not an odd minute.")
-today = datetime.today()
-if today == 'Saturday':
-    print("Hurray!")
-elif today == 'Monday':
-    print("Oh my God...")
-else:
-    print("Just a usual day.")
+# repeat the result three times
+for i in range(3):
+
+    right_this_minute = datetime.today().minute
+    if right_this_minute in odds:
+        print("This minute seems odd.")
+    else:
+        print("Not an odd minute.")
+    today = datetime.today()
+    if today == 'Saturday':
+        print("Hurray!")
+    elif today == 'Monday':
+        print("Oh my God...")
+    else:
+        print("Just a usual day.")
+    wait_time = random.randint(1, 60)
+    time.sleep(wait_time)
